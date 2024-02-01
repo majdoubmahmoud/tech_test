7dcf8629c83f3f9601773b2764daee3a97ba939e
Majdoub Mahmoud <132488428+majdoubmahmoud@users.noreply.github.com>
2024-02-01 18:49:04 +0100

    Update haproxy.py

--- haproxy.py
+++ haproxy.py
@@ -5,7 +5,7 @@ def install_haproxy():
     subprocess.run(["sudo", "apt", "install", "haproxy", "-y"])
 
 def configure_haproxy(haproxy_port, server_name, backend_servers):
-    # Customize your HAProxy configuration here if needed
+    #haproxy config file
     haproxy_config = f"""
     global
         daemon
@@ -27,7 +27,6 @@ def configure_haproxy(haproxy_port, server_name, backend_servers):
     for i, (backend_ip, backend_port) in enumerate(backend_servers, start=1):
         haproxy_config += f"        server server{i} {backend_ip}:{backend_port} check\n"
 
-    # Add a newline character at the end of the configuration
     haproxy_config += "\n"
 
     with open("/etc/haproxy/haproxy.cfg", "w") as config_file:
@@ -38,26 +37,26 @@ def configure_haproxy(haproxy_port, server_name, backend_servers):
 if __name__ == "__main__":
     install_haproxy()
 
-    # Get user input for HAProxy port, server name, and backend servers
-    haproxy_port = input("Enter the HAProxy port (default is 80): ") or "80"
-    server_name = input("Enter the server name for HAProxy (e.g., example.com): ")
+    #interactions
+    haproxy_port = input("enter the haproxy port (default is 80): ") or "80"
+    server_name = input("enter the server name for haproxy: ")
 
     backend_servers = []
-    num_backends = int(input("Enter the number of backend servers: "))
+    num_backends = int(input("enter the number of backend servers: "))
     for i in range(num_backends):
-        backend_ip = input(f"Enter the IP address of backend server {i+1}: ")
-        backend_port = input(f"Enter the port for backend server {i+1} (default is {haproxy_port}): ") or haproxy_port
+        backend_ip = input(f"enter the IP address of backend server {i+1}: ")
+        backend_port = input(f"enter the port for backend server {i+1} (default is {haproxy_port}): ") or haproxy_port
         backend_servers.append((backend_ip, backend_port))
 
     configure_haproxy(haproxy_port, server_name, backend_servers)
 
-    # Get user input for firewall configuration
-    firewall_enable = input("Do you want to enable the firewall? (y/n): ").lower()
+    #firewall configuration
+    firewall_enable = input("do you want to enable the firewall? (y/n): ").lower()
     if firewall_enable == "y":
         subprocess.run(["sudo", "ufw", "allow", haproxy_port])
         subprocess.run(["sudo", "ufw", "enable"])
         print(f"Firewall configured to allow traffic on port {haproxy_port}.")
     else:
-        print("Firewall not enabled.")
+        print("firewall not enabled")
 
-    print("Load balancer setup completed.")
+    print("load balancer setup completed")
